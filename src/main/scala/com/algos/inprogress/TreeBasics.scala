d20ea91e809a5f6bf891f4c06fe648ba8d3d67a9
Max Wittman <Max.Wittman@redballoon.com.au>
2016-01-23 20:39:32 +1100

    Tree traverses almost working in FP style

--- src/main/scala/com/algos/inprogress/TreeBasics.scala
+++ src/main/scala/com/algos/inprogress/TreeBasics.scala
@@ -4,6 +4,11 @@ import com.algos.ds.Node
 
 object TreeBasics {
   def main(args: Array[String]): Unit = {
+    println(List(1, 2, 3) ::: List(4, 5, 6) ::: List(7, 8, 9))
+    println(List(1, 2, 3) ::: (List(4, 5, 6) ::: List(7, 8, 9)))
+    println((List(1, 2, 3) ::: List(4, 5, 6)) ::: List(7, 8, 9))
+
+
     val tree =
       (Node(1) /\
         (Node(2) /\
@@ -11,10 +16,15 @@ object TreeBasics {
             Node(6))
           (Node(4)))(
         Node(5))
+    //3, 6, 4, 1, 5
 
-    print(treeToString(tree))
+    println(treeToString(tree))
 
-    print(height(tree))
+    println(height(tree) + ", " + heightViaTraverseViaTraverse2(tree))
+
+    println(preorder(tree))
+    println(inorder(tree))
+    println(postorder(tree))
   }
 
   def treeToString[A](cur: Node[A]): String = {
@@ -30,23 +40,68 @@ object TreeBasics {
     go(cur, "") + "\n"
   }
 
-  def traverse[A, B](cur: Node[A], f: A => B, fParentChild: (B, B) => B, fBranch: (B, B) => B): B = {
-    def t = (node: Node[A]) => traverse(node, f, fParentChild, fBranch)
-
+  def traverse[A, B](cur: Node[A], fab: A => B, f: (B, Option[B], Option[B]) => B): B = {
     cur match {
-      case Node(value, Some(lhs), Some(rhs)) => fParentChild(f(value), fBranch(t(lhs), t(rhs)))
-      case Node(value, Some(lhs), None) => fParentChild(f(value), t(lhs))
-      case Node(value, None, Some(rhs)) => fParentChild(f(value), t(rhs))
-      case Node(value, None, None) => f(value)
+      case Node(value, Some(lhs), Some(rhs)) => f(fab(value), Some(traverse(lhs, fab, f)), Some(traverse(rhs, fab, f)))
+      case Node(value, Some(lhs), None) => f(fab(value), Some(traverse(lhs, fab, f)), None)
+      case Node(value, None, Some(rhs)) => f(fab(value), None, Some(traverse(rhs, fab, f)))
+      case Node(value, None, None) => fab(value)
     }
   }
 
-  def height[A](cur: Node[A]): Int = cur match {
-    case Node(_, Some(lhs), Some(rhs)) => 1 + Math.max(height(lhs), height(rhs))
-    case Node(_, Some(lhs), None) => 1 + height(lhs)
-    case Node(_, None, Some(rhs)) => 1 + height(rhs)
-    case Node(_, None, None) => 1
+  def traverse2[A, B](cur: Node[A], f: A => B, fParentChild: (B, B) => B, fBranch: (B, B) => B): B = {
+    traverse(cur, f, (v: B, lhsO: Option[B], rhsO: Option[B]) => {
+      lhsO.fold(
+        rhsO.fold(v)((rhs: B) => fParentChild(v, rhs))
+      )((lhs: B) =>
+        rhsO.fold(fParentChild(v, lhs))((rhs: B) => fParentChild(v, rhs))
+      )
+
+//      val fa = f(cur.value)
+//      if (lhsO.isDefined && rhsO.isDefined) {
+//        fParentChild(fa, fBranch(lhsO.get, rhsO.get))
+//      } else if (lhsO.isDefined) {
+//        fParentChild(fa, lhsO.get)
+//      } else if (rhsO.isDefined) {
+//        fParentChild(fa, rhsO.get)
+//      } else {
+//        fa
+//      }
+    })
   }
 
-  def preorder[A]
+  def height[A](cur: Node[A]): Int =
+    traverse2[A, Int](cur, _ => 1, _ + _, (a, b) => Math.max(a, b))
+
+  def heightViaTraverseViaTraverse2[A](cur: Node[A]): Int =
+    traverse2[A, Int](cur, _ => 1, _ + _, (a, b) => Math.max(a, b))
+
+  def inorder[A](cur: Node[A]): List[A] = {
+    traverse[A, List[A]](cur, i => List(i), (p, lhsO, rhsO) => {
+      val lhs: List[A] = lhsO.getOrElse(Nil)
+      val rhs: List[A] = rhsO.getOrElse(Nil)
+      val result: List[A] = lhs ::: p ::: rhs
+      result
+    })
+  }
+
+  //1, 2, 3, 6, 4, 5
+  def preorder[A](cur: Node[A]): List[A] = {
+    traverse[A, List[A]](cur, i => List(i), (p, lhsO, rhsO) => {
+      val lhs: List[A] = lhsO.getOrElse(Nil)
+      val rhs: List[A] = rhsO.getOrElse(Nil)
+      val result: List[A] = p ::: lhs ::: rhs
+      result
+    })
+  }
+
+  //6, 3, 4, 2, 5, 1
+  def postorder[A](cur: Node[A]): List[A] = {
+    traverse[A, List[A]](cur, i => List(i), (p, lhsO, rhsO) => {
+      val lhs: List[A] = lhsO.getOrElse(Nil)
+      val rhs: List[A] = rhsO.getOrElse(Nil)
+      val result: List[A] = lhs ::: rhs ::: p
+      result
+    })
+  }
 }
